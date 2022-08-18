56fb22d17a6aef1bf759e3bd8e233b276ff4c8a0
sahinmeric <sahin.meric86@gmail.com>
2022-08-18 20:44:51 -0300

    static file dir added for nginx

--- ahorrapp/ahorrapp/settings.py
+++ ahorrapp/ahorrapp/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = "/var/www/ahorrapp/static"
+STATICFILES_DIRS = [BASE_DIR / "ahorrapp" / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
