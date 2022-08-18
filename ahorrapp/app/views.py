b60a8e4d4b709ef00f000316c59e7beee7c3a769
sahinmeric <sahin.meric86@gmail.com>
2022-08-18 18:50:02 -0300

    static files conf

--- ahorrapp/app/views.py
+++ ahorrapp/app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("this is home page")
+    return render(request, "home.html")
+    # return HttpResponse("this is home page")
