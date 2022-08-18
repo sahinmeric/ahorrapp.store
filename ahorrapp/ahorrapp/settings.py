1e86bf47211e7cfdd2b5b9be7e48b884dfb110e4
sahinmeric <sahin.meric86@gmail.com>
2022-08-18 20:35:15 -0300

    static image test

--- ahorrapp/ahorrapp/settings.py
+++ ahorrapp/ahorrapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ahorrapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
