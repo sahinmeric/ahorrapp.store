5b6a607beeac447ff848065c048fe34128ce3541
sahinmeric <sahin.meric86@gmail.com>
2022-09-09 21:23:31 -0300

    db creds moved to env

--- ahorrapp/ahorrapp/settings.py
+++ ahorrapp/ahorrapp/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'ahorrapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ahorrapp',
-        'USER': 'sahin',
-        'PASSWORD': 'Aq!sahin4354',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
